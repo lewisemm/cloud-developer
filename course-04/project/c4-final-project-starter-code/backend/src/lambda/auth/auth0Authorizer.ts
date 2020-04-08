76f50d95329b12ae66aa42e64418884dde0d02ce
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-04-08 21:31:26 +0300

    Implement auth function
    - Auth function uses the RS256 algorithm to verify tokens.
    - Fetch certificate from auth0 endpoint and use it to verify
    token via asymetric algorithm

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,22 +1,19 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
 
-// TODO: Provide a URL that can be used to download a certificate that can be used
+// Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-0s9t70t5.auth0.com/.well-known/jwks.json'
 
-export const handler = async (
-  event: CustomAuthorizerEvent
-): Promise<CustomAuthorizerResult> => {
+export const handler = async (event: CustomAuthorizerEvent): Promise<CustomAuthorizerResult> => {
   logger.info('Authorizing a user', event.authorizationToken)
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
@@ -56,12 +53,22 @@ export const handler = async (
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+
+  const jwks = await Axios({
+    method: 'GET',
+    url: jwksUrl,
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+
+  const certificate = certToPEM(jwks.data.keys[0].x5c[0])
+  logger.info('Certificate retrieved', certificate)
+
+  return verify(token, certificate, { algorithms: ['RS256']}) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +81,10 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
 }
