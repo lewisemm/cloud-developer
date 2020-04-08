cd99ef5c085ce96fa591ebb216b20f0f4b649c48
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-04-08 12:15:56 +0300

    Implement first version of deleteTodo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/deleteTodo.ts
@@ -1,10 +1,63 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import { createLogger } from '../../utils/logger'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todoTable = process.env.TODO_TABLE
+const logger = createLogger('deleteTodos')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const todoId = event.pathParameters.todoId
 
+  const validTodo = todoExists(todoId)
+
+  if (!validTodo) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({
+        error: 'Todo item does not exist'
+      })
+    }
+  }
+
   // TODO: Remove a TODO item by id
-  return undefined
+  const params = {
+    TableName:todoTable,
+    Key: {
+      todoId
+    },
+    ReturnValues:"NONE"
+  };
+
+  logger.info(`Attempting to delete todo item of todoId: ${todoId}.`)
+
+  const deletedResult = await docClient.delete(params).promise()
+
+  return {
+    statusCode: 204,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      deletedResult
+    })
+  }
+}
+
+
+async function todoExists(todoId: string) {
+  const result = await docClient
+    .get({
+      TableName: todoTable,
+      Key: {
+        todoId
+      }
+    })
+    .promise()
+
+  logger.info('Get todo: ', result)
+  return !!result.Item
 }
