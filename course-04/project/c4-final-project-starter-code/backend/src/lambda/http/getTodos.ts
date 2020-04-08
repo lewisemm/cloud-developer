ecce64394d7c9141b92e8f49e66c37190871e29d
Lewis Kabui <lewisemm@users.noreply.github.com>
2020-04-08 12:19:35 +0300

    Implement first pass of getTodo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/getTodos.ts
@@ -1,7 +1,30 @@
 import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS  from 'aws-sdk'
+import { createLogger } from '../../utils/logger'
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+const todosTable = process.env.TODO_TABLE
+const logger = createLogger('getTodos')
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  logger.info('Processing event: ', event)
+
+  const result = await docClient.scan({
+    TableName: todosTable
+  }).promise()
+
+  const items = result.Items
+  logger.info('Todo items retrieved')
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
+  }
 }
